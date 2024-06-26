ce6b99109c63182164f3e5c3f6b957a4f591d24f
gtwhhhh <93729382+ic1414@users.noreply.github.com>
2024-06-26 17:55:51 +0800

    use linked list instead of tree

--- src/mymenu.cpp
+++ src/mymenu.cpp
@@ -1,109 +1,106 @@
 #include "mymenu.h"
 
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
 
 
-void MYMENU::init(void (*func_4in1)(int16_t)){ ptr_4in1 = func_4in1; }
 
-void MYMENU::showOnDisplay1(){ (*ptr_4in1)(1);}
-void MYMENU::showOnDisplay2(){ (*ptr_4in1)(2); }
-void MYMENU::function_setup(){ (*ptr_4in1)(3); }
-void MYMENU::function_loop(){ (*ptr_4in1)(4); }
-void MYMENU::function_exit(){( *ptr_4in1)(5); }
-
-void MYMENU::activate_node()  { menu_state = 1;    }
-void MYMENU::deactivate_node(){ menu_state = 0;    }
-bool MYMENU::is_activated()   { return menu_state; }
-
-int16_t MYMENU::get_num_valid_submenus(){ return num_valid_submenus; }
-
-void MYMENU::set_submenu(MYMENU *m1){ submenu_list[0] = m1; num_valid_submenus = 1; }
-void MYMENU::set_submenu(MYMENU *m1, MYMENU *m2){ set_submenu(m1); submenu_list[1] = m2; num_valid_submenus = 2; }
-void MYMENU::set_submenu(MYMENU *m1, MYMENU *m2, MYMENU *m3){ set_submenu(m1, m2); submenu_list[2] = m3; num_valid_submenus = 3; }
-void MYMENU::set_submenu(MYMENU *m1, MYMENU *m2, MYMENU *m3, MYMENU *m4){ set_submenu(m1, m2, m3); submenu_list[3] = m4; num_valid_submenus = 4; }
-void MYMENU::set_submenu(MYMENU *m1, MYMENU *m2, MYMENU *m3, MYMENU *m4, MYMENU *m5){ set_submenu(m1, m2, m3, m4); submenu_list[4] = m5; num_valid_submenus = 5; }
-void MYMENU::set_submenu(MYMENU *m1, MYMENU *m2, MYMENU *m3, MYMENU *m4, MYMENU *m5, MYMENU *m6){ set_submenu(m1, m2, m3, m4, m5); submenu_list[5] = m6; num_valid_submenus = 6; }
-void MYMENU::set_submenu(MYMENU *m1, MYMENU *m2, MYMENU *m3, MYMENU *m4, MYMENU *m5, MYMENU *m6, MYMENU *m7){ set_submenu(m1, m2, m3, m4, m5, m6); submenu_list[6] = m7; num_valid_submenus = 7; }
-
-
-void NAVI::init(MYMENU *starting_menu, MYMENU* exit_menu, AiEsp32RotaryEncoder *rotaryEncoder){
-    primitive = starting_menu;
-    // use submenu0(mymenu_home) of mymenu_black when boot
-    starting_menu->submenu_list[0]->activate_node(); 
-    current_menu = starting_menu->submenu_list[0];
-    exitt = exit_menu;
-    encoder = rotaryEncoder;
-    encoder->setBoundaries(0, current_menu->get_num_valid_submenus()-1, 1);
-    encoder->reset(0); // reset the encoder to prevent a[4] = 5 for (a[2])
+// init
+void MYMENU::init(void (*func_5in1)(int16_t))
+{
+    ptr_5in1 = func_5in1;
 }
 
 
-MYMENU* NAVI::find_offspring(MYMENU *amenu){
+// 功能函数中的case1。
+void MYMENU::showOnDisplay1(){(*ptr_5in1)(1);}
 
-    for(int16_t i=0; i<amenu->get_num_valid_submenus(); i++){
-        if(amenu->submenu_list[i] != NULL) if(amenu->submenu_list[i]->is_activated() == 1) return find_offspring(amenu->submenu_list[i]);
+// 功能函数中的case2。
+void MYMENU::showOnDisplay2(){ (*ptr_5in1)(2); }
+
+// 功能函数中的case3。
+void MYMENU::function_setup(){ (*ptr_5in1)(3); }
+
+// 功能函数中的case4。
+void MYMENU::function_loop(){ (*ptr_5in1)(4); }
+
+// 功能函数中的case5。
+void MYMENU::function_exit(){( *ptr_5in1)(5); }
+
+
+// get number of valid submenus
+int16_t MYMENU::get_num_valid_submenus(){
+    return num_valid_submenus;
+}
+
+
+// 设置菜单的子菜单
+void MYMENU::set_submenu(MYMENU * array[], uint8_t arr_size){
+    // 在这里，array 是外部数组的起始内存地址。 sizeof(array) == 0，需要外部传进 arr_size。
+    num_valid_submenus = arr_size;
+
+    // 不能超过定义中最多子菜单的数量
+    if(num_valid_submenus > mymenu_max_subs){
+        for(;;){
+            Serial.print("exceed max sub menu size !!!!!!");
+            delay(100);
+        }
     }
-    return amenu;
+
+    for(uint8_t i=0; i<num_valid_submenus; i++){
+        array[i]->parent_menu = this; // 将当前菜单设为数组里菜单的父级菜单
+        submenu_list[i] = array[i]; // 将子菜单链接到父级菜单
+    }
+
 }
 
 
-#define time_update_delay 50
-#define time_button_delay 400
+void NAVI::init(MYMENU *start_menu, MYMENU* exit_menu, AiEsp32RotaryEncoder *rotaryEncoder){
+    current_menu = start_menu; // 开机的时候进入到 start_menu
+    exitt = exit_menu; // 让 导航 知道什么是<退出>菜单
+    encoder = rotaryEncoder; // 让 导航 能获得编码器的值
+    encoder->setBoundaries(0, current_menu->get_num_valid_submenus()-1, 1); // 设置编码器的范围
+    encoder->reset(0); // reset the encoder to prevent index out of range.
+}
 
+
+#define time_update_delay 50 // 每隔x毫秒刷新一次屏幕（除了刷新屏幕你也可以做其他的任务）
+#define time_button_delay 400 // 连续两次按下编码器最少间隔x毫秒，用来防噪。
+
+// 菜单导航更新。你的所有功能函数将在这里面被触发。
 void NAVI::update(){
-    static unsigned long timer_update = millis();
-    static unsigned long timer_lastpress = millis();
+    static unsigned long timer_update = millis(); // 屏幕刷新 计时器
+    static unsigned long timer_lastpress = millis(); // 上次按下编码器时间 计时器
 
+    // 更新两个屏幕的画面
     if(millis() - timer_update >= time_update_delay){
         timer_update = millis();
 
         current_menu->showOnDisplay1();
         current_menu->submenu_list[encoder->readEncoder()]->showOnDisplay2();
     }
-    
+
     if (encoder->isEncoderButtonClicked() and (millis() - timer_lastpress > time_button_delay)){
         timer_lastpress = millis();
+
+        // 如果选择中的子菜单是<退出>菜单
         if(current_menu->submenu_list[encoder->readEncoder()] == exitt)
         {
             exitt->function_setup();
             current_menu->function_exit();
-            current_menu->deactivate_node();
+            current_menu = current_menu->parent_menu; // 导航到当前菜单的父级菜单
         }
-        else (current_menu->submenu_list[encoder->readEncoder()])->activate_node();
-        current_menu = find_offspring(primitive);
-        encoder->setBoundaries(0, current_menu->get_num_valid_submenus() - 1, 1);
-        encoder->reset(0); // reset the encoder to prevent a[4] = 5 for (a[2])
+        else{
+            // 导航到选中的子菜单
+            current_menu = current_menu->submenu_list[encoder->readEncoder()];
+        }
+
+        encoder->setBoundaries(0, current_menu->get_num_valid_submenus() - 1, 1); // prevent index out of range
+        encoder->reset(0); // reset the encoder to prevent referring arr[4] (length(arr) == 2).
         current_menu->function_setup();
     }
 
     current_menu->function_loop();
-    
+
 }
 
 
 
-/*
-void MYMENU::add_submenu(MYMENU *menu){
-    submenu_list[num_valid_submenus] = menu;
-    num_valid_submenus += 1;
-}
-*/
-
-/*
-MYMENU* NAVI::find_offspring(MYMENU *amenu){
-    bool the_end = 0;
-    uint8_t count = 0;
-    while(the_end == 0){
-        for(int16_t i=0; i<amenu->get_num_valid_submenus(); i++){
-            if(amenu->submenu_list[i] != NULL) if(amenu->submenu_list[i]->is_activated() == 1){
-                amenu = amenu->submenu_list[i];
-                count = 0;
-            }else count += 1;
-        }
-        if(count == amenu->get_num_valid_submenus()) break;
-    }
-    return amenu;
-}
-*/
--- src/mymenu.h
+++ src/mymenu.h
@@ -3,22 +3,24 @@
 
 #include <Arduino.h>
 #include <AiEsp32RotaryEncoder.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
 
-#define mymenu_max_subs 7 // max submenu per menu
 
+// 每个目录最多的子菜单数量
+#define mymenu_max_subs 7
+
+// 菜单
 class MYMENU
 {
 private:
-    bool menu_state = 0;
-    uint8_t num_valid_submenus = 0;
-    void (*ptr_4in1)(int16_t) = NULL;
+    uint8_t num_valid_submenus = 0; //当前菜单的可用子菜单数量
+    void (*ptr_5in1)(int16_t) = NULL; // 功能函数。
 
-public:    
+public:
     void init(void (*)(int16_t));
-    bool is_activated();
-    void activate_node();
-    void deactivate_node();
 
     void showOnDisplay1();
     void showOnDisplay2();
@@ -26,27 +28,28 @@ public:
     void function_loop();
     void function_exit();
 
-    // void add_submenu(MYMENU *); abandoned
-    void set_submenu(MYMENU *); void set_submenu(MYMENU *, MYMENU *); void set_submenu(MYMENU *, MYMENU *, MYMENU *);
-    void set_submenu(MYMENU *, MYMENU *, MYMENU *, MYMENU *); void set_submenu(MYMENU *, MYMENU *, MYMENU *, MYMENU *, MYMENU *);
-    void set_submenu(MYMENU *, MYMENU *, MYMENU *, MYMENU *, MYMENU *, MYMENU *); void set_submenu(MYMENU *, MYMENU *, MYMENU *, MYMENU *, MYMENU *, MYMENU *, MYMENU *);
+    MYMENU *parent_menu = NULL; // 菜单的父级菜单
+    MYMENU *submenu_list[mymenu_max_subs] = {NULL}; // 子菜单数组
+    
     int16_t get_num_valid_submenus();
-    MYMENU *submenu_list[mymenu_max_subs] = {NULL};  
+    void set_submenu(MYMENU * array[], uint8_t);
 };
 
 
+// 菜单导航。（navi - navigation）
 class NAVI
 {
 private:
-    MYMENU *primitive = NULL;
-    MYMENU *exitt = NULL;
-    MYMENU *current_menu = NULL;
-    MYMENU *find_offspring(MYMENU *);
-    AiEsp32RotaryEncoder *encoder;
+    MYMENU *exitt = NULL; // <退出>菜单。当导航到<退出>的时候则回到当前菜单的父级菜单
+    MYMENU *current_menu = NULL; // 最新被选中的菜单，当一个菜单是 current_menu 时它的功能函数将被执行
+    AiEsp32RotaryEncoder *encoder; // 旋转编码器
 
 public:
     void init(MYMENU *, MYMENU *, AiEsp32RotaryEncoder *);
     void update();
 };
+
+
+
 
 #endif
